d861d62c70967479c477d6c9b19425c816aa0453
Jéssica Pinheiro <jessicasilva.js1314@gmail.com>
2025-09-15 14:03:32 -0300

    Correcting number type for property quantity

--- backend/src/schemas/entryDate.ts
+++ backend/src/schemas/entryDate.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 
 export const productSchema = z.object({
     name: z.string().min(4, 'The name is mandatory'),
-    quantity: z.number().negative().positive('The quantity is mandatory'),
+    quantity: z.number().positive().positive('The quantity is mandatory'),
     price: z.number().positive("O preço deve ser maior que 0"),
     provider: z.string().min(1, "O fornecedor é obrigatório"),
 });
